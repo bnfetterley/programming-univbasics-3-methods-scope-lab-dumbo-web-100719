6b591ce3124d37c618b261254e626be630a71730
Brianna Fetterley <bnfetterley@usfca.edu>
2019-09-22 22:56:00 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,29 @@
+
+
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  phrase = "Its-a me, Mario!"
   puts phrase
 end
 
+mario
+
 def toadstool
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
+end
+
+toadstool
+
+def link 
+  takethisphrase = "It's Dangerous To Go Alone! Take This."
+  puts takethisphrase
+end
+
+link
+
+def any_phrase(phrase)
+  print #{phrase}
+
+any_phrase("Do a Barrel Roll!\n")
+  
 end
